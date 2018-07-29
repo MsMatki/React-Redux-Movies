4b2022118045eec524847e28043793c5aa57077f
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-07-28 23:06:05 -0400

    add fetch APIs to fetch data from themoviedb.org

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Movies from './movieContainer'
 import {DebounceInput} from 'react-debounce-input';
 
+
+
+const api = 'https://api.themoviedb.org/3'
+const apiKey = 'a9632aa4c0a084cd40a2f5f911739ec0'
+
 class App extends Component {
 
   state = {
@@ -27,10 +32,7 @@ class App extends Component {
       })
     }else{
 
-    const api = 'https://api.themoviedb.org/3'
-    const apiKey = 'a9632aa4c0a084cd40a2f5f911739ec0'
     const url = `${api}/search/movie?api_key=${apiKey}&query=${query}`
-
     fetch(url)
     .then(response => response.json())
     
@@ -47,6 +49,53 @@ class App extends Component {
   }
   }
 
+  mostPopular = () => {
+
+    const url = `${api}/discover/movie?api_key=${apiKey}&?sort_by=popularity.desc`
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+    this.setState({searchedMovies: data.results})
+    console.log(data.results)
+    }).catch(error => console.log('Cant fetch any data', error))
+  }
+  
+  
+  upcoming = () => {
+    const url = `${api}/discover/movie?api_key=${apiKey}&primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22`
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+    this.setState({searchedMovies: data.results})
+    }).catch(error => console.log('Cant fetch any data', error))
+  }
+
+  topRated = () => {
+    const url = `${api}/discover/movie?api_key=${apiKey}&certification_country=US&certification=R&sort_by=vote_average.desc`
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+    this.setState({searchedMovies: data.results})
+    }).catch(error => console.log('Cant fetch any data', error))
+  }
+
+  kidsPopular = () => {
+    const url = `${api}/discover/movie?api_key=${apiKey}&certification_country=US&certification.lte=G&sort_by=popularity.desc`
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+    this.setState({searchedMovies: data.results})
+    }).catch(error => console.log('Cant fetch any data', error))
+  }
+
+  dramas = () => {
+    const url = `${api}/discover/movie?api_key=${apiKey}&with_genres=18&sort_by=vote_average.desc&vote_count.gte=10`
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+    this.setState({searchedMovies: data.results})
+    }).catch(error => console.log('Cant fetch any data', error))
+  }
 
   openCloseMenu = () => {
     let ul = document.querySelector('.topnav')
@@ -81,10 +130,11 @@ class App extends Component {
               onChange={(event) => this.updateQuery(event.target.value)}/>
               
             </li>
-            <li><a href="#about" className="tag">Upcoming</a></li>
-            <li><a href="#skills" className="tag">Top Rated</a></li>
-            <li><a href="#portfolio" className="tag">Popular</a></li>
-            <li><a href="#contact" className="tag">Latest</a></li>
+            <li><a href="#about" className="tag" onClick={(event) => this.upcoming(event.target.value)}>Upcoming</a></li>
+            <li><a href="#skills" className="tag" onClick={(event) => this.topRated(event.target.value)}>Top Rated</a></li>
+            <li><a href="#portfolio" className="tag"  onClick={(event) => this.mostPopular(event.target.value)}>Popular</a></li>
+            <li><a href="#contact" className="tag" onClick={(event) => this.kidsPopular(event.target.value)}>Kids</a></li>
+            <li><a href="#contact" className="tag" onClick={(event) => this.dramas(event.target.value)}>Drama</a></li>
           </ul>
         </nav>
       </div>
