99bfea0088df7cd39181a77574ac452df89aadbb
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-08-01 23:23:53 -0400

    add function to fetch movie details from moviedb.org

--- src/Movie.js
+++ src/Movie.js
@@ -1,19 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+const api = 'https://api.themoviedb.org/3'
+const apiKey = 'a9632aa4c0a084cd40a2f5f911739ec0'
 
-export default class Movies extends Component{
+export default class Movie extends Component {
+
+
+    state = {
+        movieInfo:[]
+    }
+
+    movieInfo = () => {
+        const {movie} = this.props;
+        const {movieInfo} = this.state;
+
+        const url = `${api}/movie/${movie.id}?api_key=${apiKey}&append_to_response=videos,images`
+        fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+        this.setState({movieInfo: movieInfo.push(data)})
+        console.log(movieInfo)
+        }).catch(error => console.log('Cant fetch any data', error))
+
+        }
 
 
 
     render(){
-        const {movie} = this.props
+        const {movie} = this.props;
         let poster = `http://image.tmdb.org/t/p/w185/${movie.poster_path}`
         
        const styles = {width: '100%', height: '100%', backgroundImage:poster ? (`url(${poster})`) : null }
         return(
-            <div className="movie-item">
-                
+            <Link to="/MovieInfo">
+            <div className="movie-item" onClick={this.movieInfo}>
                     <div className="movie-cover" style={styles}>
                         <div className="movie-rating"><span className="star"><FontAwesomeIcon icon={faStar}/></span><h6>{movie.vote_average}</h6></div>
                     </div>
@@ -21,8 +43,11 @@ export default class Movies extends Component{
                         <h6>{movie.title}{movie.name}</h6>
                     </div>
             </div>
+            </Link>
         )
     }
 }
+
+
 
 /*http://image.tmdb.org/t/p/w185//nLRcqfvSYj2AKy9aG10Dx6bxUsS.jpg*/
