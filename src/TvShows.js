3e75222faa596e31ff3179d02b6a8e208d46f071
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-08-08 01:01:58 -0400

    Remove fetching data functions, import from actions

--- src/TvShows.js
+++ src/TvShows.js
@@ -3,19 +3,17 @@ import Header from './Header'
 import GenreTv from './GenreTvContainer'
 import MovieContainer from './MovieContainer'
 import $ from "jquery";
+import {connect} from 'react-redux'
+import {tvPopular, tvTopRated, tvOnTheAir, airingToday, searchTvShows} from './actions/movieActions'
+
 
 const api = 'https://api.themoviedb.org/3'
 const apiKey = 'a9632aa4c0a084cd40a2f5f911739ec0'
 
-export default class TvShows extends Component{
-
-    state = {
-        query:'',
-        searchedTvShows:[]
-      }
+class TvShows extends Component{
     
       componentDidMount(){
-        this.mostPopular()
+        this.props.tvPopular()
         this.resetQuery();
       }
 
@@ -25,7 +23,7 @@ export default class TvShows extends Component{
       }
       
       setActive = () => {
-          let {query} = this.state
+          let {query} = this.props
               if(query !== ''){
                 $('.list .active').removeClass('active');
               }
@@ -46,88 +44,27 @@ export default class TvShows extends Component{
         })
       }
 
-      updateQuery = (query) => {
-        this.setState({
-          query: query
-        })
-        this.searchTvShows()
-        
-      }
-
-    searchTvShows = () => {
-        const {query} = this.state;
-      //if query is empty set default upcoming movies
-      if(query === ''){
-        this.mostPopular()
-      }else{
-      const url = `${api}/search/tv?api_key=${apiKey}&query=${query}`
-      fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        //remove error on empty string, and set movies to default upcoming
-      if(data.results.error){
-          this.mostPopular()
-      }else{
-      this.setState({searchedTvShows: data.results})
-      console.log(this.state.searchedTvShows)
-      }
-      }).catch(error => console.log('Cant fetch any data', error))
-    }
-    }
-
-    mostPopular = () => {
-        $('.card').addClass('active')
-        const url = `${api}/tv/popular?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedTvShows: data.results})
-        console.log(data.results)
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-      
-    
-      topRated = () => {
-        const url = `${api}/tv/top_rated?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedTvShows: data.results})
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-
-      tvOnTheAir = () => {
-        const url = `${api}/tv/on_the_air?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedTvShows: data.results})
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-
-      airingToday = () => {
-        const url = `${api}/tv/airing_today?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedTvShows: data.results})
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
 
 
     render(){
         return(
             <div>
-                <Header query={this.state.query} updateQuery={this.updateQuery}/>
+                <Header query={this.props.query} searchData={this.props.searchTvShows}/>
                 <GenreTv 
-                    mostPopular={this.mostPopular}
-                    topRated={this.topRated}
-                    tvOnTheAir={this.tvOnTheAir}
-                    airingToday={this.airingToday}
+                    tvPopular={this.props.tvPopular}
+                    tvTopRated={this.props.tvTopRated}
+                    tvOnTheAir={this.props.tvOnTheAir}
+                    airingToday={this.props.airingToday}
                     setActive={this.setActive}
                     />
-                <MovieContainer searchedMovies={this.state.searchedTvShows}/>
+                <MovieContainer movies={this.props.movies}/>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => ({
+  movies: state.movies.searchedTvShows,
+})
+
+export default connect(mapStateToProps, {tvPopular, tvTopRated, tvOnTheAir, airingToday, searchTvShows})(TvShows)
