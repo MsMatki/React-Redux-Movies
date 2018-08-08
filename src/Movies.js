10265b2eb66a5d6e6dd00eb84decf215b3ccbb28
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-08-08 01:00:16 -0400

    remove fetching data functions

--- src/Movies.js
+++ src/Movies.js
@@ -4,20 +4,21 @@ import MovieContainer from './MovieContainer'
 import Header from './Header'
 import GenreMovie from './GenreMovieContainer'
 import $ from "jquery";
+import {connect} from 'react-redux'
+import {mostPopular, upcoming, topRated, kidsPopular, nowPlaying, searchMovies} from './actions/movieActions'
 
 const api = 'https://api.themoviedb.org/3'
 const apiKey = 'a9632aa4c0a084cd40a2f5f911739ec0'
 
-export default class Movies extends Component{
+class Movies extends Component{
 
-    state = {
-        query:'',
-        searchedMovies:[]
-      }
+   state = {
+     query:''
+   }
     
     componentDidMount(){
       //set upcoming movies as a default
-      this.upcoming()
+      this.props.upcoming()
       this.resetQueryOnClick()
     
     }
@@ -53,102 +54,32 @@ export default class Movies extends Component{
               $(this).addClass('active');
             }) 
       }
-    
-      updateQuery = (query) => {
-        this.setState({
-          query: query
-        })
-        this.searchMovies()
-        
-      }
-    
-        searchMovies = () => {
-          const {query} = this.state;
-        //if query is empty set default upcoming movies
-        if(query === ''){
-          this.upcoming()
-        }else{
-        const url = `${api}/search/movie?api_key=${apiKey}&query=${query}`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-          //remove error on empty string, and set movies to default upcoming
-        if(data.results.error){
-            this.upcoming()
-        }else{
-        this.setState({searchedMovies: data.results})
-        console.log(this.state.searchedMovies)
-        }
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-      }
-    
-      mostPopular = () => {
-        const url = `${api}/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedMovies: data.results})
-        console.log(data.results)
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-      
-      
-      upcoming = () => {
-        $('.card').addClass('active')
-        const url = `${api}/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedMovies: data.results})
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-    
-      topRated = () => {
-        const url = `${api}/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedMovies: data.results})
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-    
-      kidsPopular = () => {
-        const url = `${api}/discover/movie?api_key=${apiKey}&certification_country=US&certification.lte=G&sort_by=popularity.desc`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedMovies: data.results})
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-    
-      nowPlayng = () => {
-        const url = `${api}/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-        this.setState({searchedMovies: data.results})
-        }).catch(error => console.log('Cant fetch any data', error))
-      }
-
-
 
     render(){
         return(
             <div>
                 <Header query={this.state.query} 
-                updateQuery={this.updateQuery}
+                searchData={this.props.searchMovies}
+
                 />
                     <GenreMovie 
-                        mostPopular={this.mostPopular}
-                        upcoming={this.upcoming}
-                        topRated={this.topRated}
-                        kidsPopular={this.kidsPopular}
-                        nowPlayng={this.nowPlayng}
+                        mostPopular={this.props.mostPopular}
+                        upcoming={this.props.upcoming}
+                        topRated={this.props.topRated}
+                        kidsPopular={this.props.kidsPopular}
+                        nowPlaying={this.props.nowPlaying}
                         setActive={this.setActive}
                     />
-                <MovieContainer searchedMovies={this.state.searchedMovies}/>
+                <MovieContainer movies={this.props.movies} movieInfo={this.props.GetMovieInfo}/>
+                
             </div>
         )
     }
 }
+
+
+const mapStateToProps = state => ({
+  movies: state.movies.searchedMovies,
+})
+
+export default connect(mapStateToProps, {mostPopular, upcoming, topRated, kidsPopular, nowPlaying, searchMovies})(Movies)
