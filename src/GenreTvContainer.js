cb814a0dab7cfd4f231e5d937a5c621a70e873bf
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-08-10 17:27:23 -0400

    style: formatting no code change

--- src/GenreTvContainer.js
+++ src/GenreTvContainer.js
@@ -14,8 +14,8 @@ export default class GenreTv extends Component{
       <div className="genre-container">
         <ul className="list">
         <div className="suggestion"><h4>Suggestion</h4><span className="angle-left"><FontAwesomeIcon icon={faAngleRight}/></span></div>
-            <li className="card active" onClick={(event) => this.props.mostPopular(event.target.value)}>Popular</li>
-            <li className="" onClick={(event) => this.props.topRated(event.target.value)}>Top Rated</li>
+            <li className="card active" onClick={(event) => this.props.tvPopular(event.target.value)}>Popular</li>
+            <li className="" onClick={(event) => this.props.tvTopRated(event.target.value)}>Top Rated</li>
             <li className="" onClick={(event) => this.props.tvOnTheAir(event.target.value)}>Tv On The Air</li>
             <li className="" onClick={(event) => this.props.airingToday(event.target.value)}>Tv Airing Today</li>
         </ul>
