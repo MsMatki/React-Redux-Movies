02ce4413966e1343e41724df7521a9abc7359653
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-08-12 23:52:09 -0400

    import bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter, BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 registerServiceWorker();
