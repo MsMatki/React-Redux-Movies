16a8bde2d0248d76df19a652635ff5d3ea50a471
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-08-25 23:44:36 -0400

    Update: import store from store folder, no code change..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
-import TvShows from './TvShows'
-import Movies from './Movies'
-import MovieInfo from './MovieInfo'
+import TvShows from './components/TvShows'
+import Movies from './components/Movies'
+import MovieInfo from './components/MovieInfo'
 import {Provider} from 'react-redux'
-import store from './store'
+import store from './store/store'
 
 
 class App extends Component {
